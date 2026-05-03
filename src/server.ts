9c2582ab63e90865b2fab29d17c362db2347f892
Rahul <rahulratnam1812@gmail.com>
2026-05-03 16:27:50 +0530

    socket connection completed

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,26 @@ import { PORT } from "./config/app.config";
 import { app } from "./app";
 import socketIo, { socketIoConfig } from "./config/socket.config";
 // import database from "./models/index";
+import socketAuthMiddleware from "./middleware/socketAuthMiddleware"
+import { Socket } from "socket.io";
 
 const server = http.createServer(app);
 socketIo.attach(server, socketIoConfig);
+socketIo.use(socketAuthMiddleware);
 
 const start = async (): Promise<void> => {
   try {
+    socketIo.on("connection", (socket:Socket) => {
+      console.log("A client connected:", socket.id);
+
+      socket.on("sentMessage", (data:any) => {
+        console.log("Message received:", data);
+        socketIo.emit("newMessage", data);
+      });
+      socket.on("disconnect", () => {
+        console.log("A client disconnected:", socket.id);
+      });
+    });
     // await database.sync({ force: false });
 
     server.listen(PORT, () => {
