b30c8b50a079450e35d3a8fc600ddbb61efb49f2
Ruksana-Shaik <ruksana18112003li@gmail.com>
2026-05-09 17:38:31 +0530

    added cors.

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,15 @@
 import express from "express";
-// import { userRoute } from "./routes/userRoute";
-// import { authRoute } from "./routes/authentication";
 // import { notFound } from "./errorHandling/errorHandling";
-// import cors from "cors";
+import cors from "cors";
 
 export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 
-// app.use('/v1/users',userRoute);
-// app.use('/v1/auth',authRoute);
 
 app.get('/', async (_req, res) => {
   try {
-    // const users = await User.findAll();
     res.status(200).json({message:'customer details api is running successfully.'});
   } catch (error) {
     res.json({message:'something went wrong'})
