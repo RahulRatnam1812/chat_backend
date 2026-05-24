d8eec42bfa682ded1443a7831e71bb0405c1804a
navya <navyakuntumalla08@gmail.com>
2026-05-24 19:52:18 +0530

    socket unique id updated

--- src/middleware/socketInitialization.ts
+++ src/middleware/socketInitialization.ts
@@ -6,8 +6,8 @@ import { JWT_SECRET_KEY } from "../config/app.config";
 export const socketInitialization = (socket: Socket, next: (err?: Error) => void) => {
 
     try {
-        console.log("socket.handshake", socket.handshake.headers.authorization)
-        const token = socket.handshake.headers.authorization?.split(" ")[1] || socket.handshake.auth?.token;
+        console.log("socket.handshake", socket.handshake)
+        const token = socket.handshake.auth?.Authorization?.split(" ")[1] || socket.handshake.auth?.token;
         if (!token) {
             return next(new Error("Authorization token is required."));
         }
@@ -15,8 +15,9 @@ export const socketInitialization = (socket: Socket, next: (err?: Error) => void
         const decoded = Jwt.decode(token) as JwtPayload;
         socket.data.user = decoded;
         console.log("decoded",decoded)
-        socket.join(decoded.userId);
-        console.log("UsersId",decoded.userId)
+        // socket.join(decoded.userId);
+        socket.join(String(decoded.uniqueId));
+        console.log("uniqueId",decoded.uniqueId)
         next();
 
     } catch (error) {
