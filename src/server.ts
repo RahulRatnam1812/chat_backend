d77bd18251d34162a9832834890a188e684e10e8
navya <navyakuntumalla08@gmail.com>
2026-05-25 22:35:28 +0530

    online user socket functionality implemented

--- src/server.ts
+++ src/server.ts
@@ -4,37 +4,44 @@ import { app } from "./app";
 // import database from "./models/index";
 import { Server } from "socket.io";
 
-import {socketConfig} from "./config/socket.config";
+import { socketConfig } from "./config/socket.config";
 import { socketInitialization } from "./middleware/socketInitialization";
 
 const server = http.createServer(app);
 const io = new Server();
 io.attach(server, socketConfig);
 io.use(socketInitialization)
-
+// const onlineUsers = new Map();
+const onlineUsers: Record<string, string> = {};
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-  console.log("user", socket.data.user);
+  console.log("connectd")
+  const uniqueId = socket.data.user.uniqueId;
+  console.log("uniqueId", uniqueId);
+  console.log("socket", socket.id);
+  if(!uniqueId){
+     socket.disconnect(true);
+     return;
+  }
 
-  socket.on("send_message", (data) => {
-    console.log("message", data);
+  // onlineUsers.set(uniqueId, socket.id);
+  // console.log("onlineUsers", onlineUsers);
+  // io.emit("online_users", Array.from(onlineUsers.keys()));
+  // socket.on("disconnect", () => {
+  //   onlineUsers.delete(uniqueId);
+  //   io.emit("online_users", Array.from(onlineUsers.keys()));
+  //   console.log("User disconnected", socket.id);
+  // });
 
-    socket.emit("receive_message", {
-      message: data.message,
-      user: socket.data.user
-    });
+  onlineUsers[uniqueId] = socket.id  
+  io.emit("online_users", Object.keys(onlineUsers))
 
-    console.log("data", data.message);
-     io.to(String(data.userId)).emit("send_particular_message", {
-      message: data.message,
-    });
-  })
   socket.on("disconnect", () => {
-    console.log("User disconnected", socket.id)
+    if (onlineUsers[uniqueId] === socket.id) {
+      delete onlineUsers[uniqueId];
+      io.emit("online_users", Object.keys(onlineUsers))
+    }
   })
-
-
-})
+});
 
 const start = async (): Promise<void> => {
   try {
@@ -43,7 +50,7 @@ const start = async (): Promise<void> => {
     server.listen(PORT, () => {
       // console.timeEnd("serverStart")
       console.log(`🚀 Server is running on http://localhost:${PORT}`);
-      
+
     });
   } catch (error) {
     console.error(error);
